79452655da6d3a6fc9cd165143363586d8c850e5
mrhl082024 <mrhl082024@gmail.com>
2024-11-22 13:46:38 +0100

    fått laget en todo list

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-function addToList() {}
+const input = document.getElementById("input-field");
+const addButton = document.getElementById("add-Btn");
+const listDisplay = document.getElementById("list-display");
+const clearListButton = document.getElementById("clear-list");
+const todoList = [];
+
+const arrayToHTML = () => {
+  for (let i = 0; i < todoList.length; i++) {
+    const node = document.createElement("p");
+    const parent = document.querySelector("#list-display");
+    node.textContent = todoList[i];
+    parent.appendChild(node);
+  }
+};
+
+function clear() {
+  document.getElementById("list-display").innerHTML = "";
+}
+
+clearListButton.onclick = () => {
+  todoList.length = 0;
+  let pList = document.querySelector("#list-display > p");
+  clear();
+
+  console.log(todoList);
+};
+
+input.addEventListener("keydown", (tast) => {
+  if (tast.key === "Enter") {
+    if (input.value !== "") {
+      todoList.push(input.value);
+      input.value = "";
+      listDisplay.textContent = "";
+      arrayToHTML();
+      console.log(todoList);
+    }
+  }
+});
+
+addButton.addEventListener("click", () => {
+  if (input.value !== "") {
+    todoList.push(input.value);
+    input.value = "";
+    listDisplay.textContent = "";
+    arrayToHTML();
+    console.log(todoList);
+  }
+});
+
+// const listObject = [
+//   {
+//     toDisplay: input.value,
+//     isCompleted: false,
+//     visible: true,
+//   },
+// ];

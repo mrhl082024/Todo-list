363e2b6c48ca70ee7fa91f7b57a29104520fdd49
mrhl082024 <mrhl082024@gmail.com>
2024-11-25 13:13:39 +0100

    laget function addtask

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const inputField = document.querySelector(".input-field");
 const addButton = document.querySelector(".add-btn");
 const clearList = document.querySelector(".clear-list");
 const listContainer = document.querySelector(".list-container");
+
+function addTask() {
+  if (inputField.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputField.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "";
+    li.appendChild(span);
+  }
+  inputField.value = "";
+  savedata();
+}

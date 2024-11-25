7aa3e52dbffec3e1b867e7bcf418cba67bfc198d
mrhl082024 <mrhl082024@gmail.com>
2024-11-25 13:05:57 +0100

    begynt å kode javascript

---
+++ index.js
@@ -0,0 +1,4 @@
+const inputField = document.querySelector(".input-field");
+const addButton = document.querySelector(".add-btn");
+const clearList = document.querySelector(".clear-list");
+const listContainer = document.querySelector(".list-container");
